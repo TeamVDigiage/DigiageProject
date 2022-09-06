f94a4b8e7080f4beb12aaf23fc889fb3f2caff72
Berk Aydemir <beerkaydemir@gmail.com>
2022-09-06 20:13:08 +0300

    Created Object Pool For Crossbow Arrows

---
+++ DigiageProject/Assets/Berk/BerkScripts/Crossbow/ObjectPool.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    private Queue<GameObject> pooledArrow;
+    [SerializeField] private GameObject arrowPrefab;
+    [SerializeField] private int poolSize;
+
+    private void Awake()
+    {
+        pooledArrow = new Queue<GameObject>();
+
+        for(int i = 0; i< poolSize; i++)
+        {
+            GameObject obj = Instantiate(arrowPrefab);
+            obj.SetActive(false);
+            pooledArrow.Enqueue(obj);
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        GameObject obj = pooledArrow.Dequeue();
+        obj.SetActive(true);
+        pooledArrow.Enqueue(obj);
+        return obj;
+    }
+}

b4bde55231e603a773b57f13a6505fa960adfe7e
Berk Aydemir <beerkaydemir@gmail.com>
2022-09-06 20:34:21 +0300

    Created and Abstract Class For Enemies

---
+++ DigiageProject/Assets/Berk/BerkScripts/Enemies/Enemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    protected int enemyHealth;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Arrow"))
+        {
+            other.gameObject.SetActive(false);
+
+            if (enemyHealth > 1)
+                enemyHealth -= 1;
+
+            else if (enemyHealth == 1)
+
+                Destroy(gameObject); //Enemy object pool yapıldığında destroy edilmeyecek, false edilecek.
+
+        }
+    }
+
+}

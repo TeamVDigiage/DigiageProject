b0075183eeb358eeff753f477f77533e39de1351
Berk Aydemir <beerkaydemir@gmail.com>
2022-09-06 20:24:37 +0300

    Added Collect and Shoot Mechanic

---
+++ DigiageProject/Assets/Berk/BerkScripts/PlayerAttack.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private ObjectPool objectPool = null;
+    [SerializeField] private int _arrowCount;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Shoot();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("CollectableArrows"))
+        {
+            _arrowCount += 4;
+            other.gameObject.SetActive(false);
+        }
+
+        Debug.Log(_arrowCount);
+    }
+
+    void Shoot()
+    {
+        if (_arrowCount > 0)
+        {
+            _arrowCount -= 1;
+            var obj = objectPool.GetPooledObject();
+            obj.transform.position = gameObject.transform.position;
+        }
+
+    }
+
+
+
+}

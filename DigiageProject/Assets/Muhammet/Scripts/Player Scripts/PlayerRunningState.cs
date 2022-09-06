de4e06ebf91b35e2d0ebad6f9e5232bae42590ca
Muhammet Fatih Yılmaz <muhammet.yilmaz.1995@gmail.com>
2022-09-06 23:51:26 +0300

    Arrange Player Running State

--- DigiageProject/Assets/Muhammet/Scripts/Player Scripts/PlayerRunningState.cs
+++ DigiageProject/Assets/Muhammet/Scripts/Player Scripts/PlayerRunningState.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerRunningState : PlayerBaseState
 {
     private float _speed = 10f;
+    private float _horizontalSpeed = 15f;
 
     public override void EnterState(PlayerStateManager state)
     {
@@ -11,10 +12,10 @@ public class PlayerRunningState : PlayerBaseState
 
     public override void UpdateState(PlayerStateManager state)
     {
-        //float horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalInput = Input.GetAxis("Horizontal");
         Vector3 forwardMove = Vector3.forward * _speed * Time.deltaTime;
-        //Vector3 horizontalMove = Vector3.right * horizontalInput * horizontalSpeed * Time.deltaTime;
-        state.GetComponent<Rigidbody>().MovePosition(state.transform.position + forwardMove); //horizontalMove
+        Vector3 horizontalMove = Vector3.right * horizontalInput * _horizontalSpeed * Time.deltaTime;
+        state.GetComponent<Rigidbody>().MovePosition(state.transform.position + forwardMove + horizontalMove); //horizontalMove
     }
 
     public override void OnTrigger(PlayerStateManager state, Collider collider)

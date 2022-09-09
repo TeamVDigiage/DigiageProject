725323391ca73b4d3d99190be6e7a62083c72c51
Muhammet Fatih Yılmaz <muhammet.yilmaz.1995@gmail.com>
2022-09-10 01:17:26 +0300

    Fixed Player Movement Borders

--- DigiageProject/Assets/Muhammet/Scripts/Player/PlayerRunningState.cs
+++ DigiageProject/Assets/Muhammet/Scripts/Player/PlayerRunningState.cs
@@ -2,24 +2,21 @@ using UnityEngine;
 
 public class PlayerRunningState : PlayerBaseState
 {
+    private Rigidbody _playerRigidbody;
     private float _speed = 15f;
-    private float _horizontalSpeed = 5f;
+    private float _horizontalSpeed = 4f;
 
     public override void EnterState(PlayerStateManager state)
     {
-        
+        _playerRigidbody = state.GetComponent<Rigidbody>();
     }
 
     public override void UpdateState(PlayerStateManager state)
     {
-        var pos = state.transform.position;
-        pos.x = Mathf.Clamp(pos.x, -1.7f, 1.7f);
-        state.transform.position = pos;
-        //
         float horizontalInput = 1 * Input.GetAxis("Horizontal");
         Vector3 forwardMove = state.transform.forward * _speed * Time.deltaTime;
         Vector3 horizontalMove = state.transform.right * horizontalInput * _horizontalSpeed * Time.deltaTime;
-        state.GetComponent<Rigidbody>().MovePosition(state.transform.position - forwardMove + horizontalMove);
+        _playerRigidbody.MovePosition(state.transform.position - forwardMove + horizontalMove);
     }
 
     public override void OnTriggerEnter(PlayerStateManager state, Collider other)
